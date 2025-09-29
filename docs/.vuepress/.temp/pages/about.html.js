6692da65e951af81bc3b132049cd17c36e1d1a3a
Bahadin <bahaking341@gmail.com>
2025-09-29 04:05:19 +0300

    add git metadata to VuePress page data for improved version tracking

--- docs/.vuepress/.temp/pages/about.html.js
+++ docs/.vuepress/.temp/pages/about.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/asus/vuepress-starter/docs/.vuepress/.temp/pages/about.html.vue"
-const data = JSON.parse("{\"path\":\"/about.html\",\"title\":\"About Me\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{},\"filePathRelative\":\"about.md\"}")
+const data = JSON.parse("{\"path\":\"/about.html\",\"title\":\"About Me\",\"lang\":\"en-US\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1759106515000,\"contributors\":[{\"name\":\"Bahadin\",\"username\":\"Bahadin\",\"email\":\"bahaking341@gmail.com\",\"commits\":1,\"url\":\"https://github.com/Bahadin\"}],\"changelog\":[{\"hash\":\"69824f9edb5831d295aebea4645208f8079798d7\",\"time\":1759106515000,\"email\":\"bahaking341@gmail.com\",\"author\":\"Bahadin\",\"message\":\"initialize VuePress site with basic configuration and deploy workflow\"}]},\"filePathRelative\":\"about.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
--- docs/.vuepress/.temp/pages/projects.html.js
+++ docs/.vuepress/.temp/pages/projects.html.js
@@ -1,5 +1,5 @@
 import comp from "C:/Users/asus/vuepress-starter/docs/.vuepress/.temp/pages/projects.html.vue"
-const data = JSON.parse("{\"path\":\"/projects.html\",\"title\":\"Projects\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Projects\"},\"headers\":[],\"git\":{},\"filePathRelative\":\"projects.md\"}")
+const data = JSON.parse("{\"path\":\"/projects.html\",\"title\":\"Projects\",\"lang\":\"en-US\",\"frontmatter\":{\"title\":\"Projects\"},\"headers\":[],\"git\":{\"updatedTime\":1759106515000,\"contributors\":[{\"name\":\"Bahadin\",\"username\":\"Bahadin\",\"email\":\"bahaking341@gmail.com\",\"commits\":1,\"url\":\"https://github.com/Bahadin\"}],\"changelog\":[{\"hash\":\"69824f9edb5831d295aebea4645208f8079798d7\",\"time\":1759106515000,\"email\":\"bahaking341@gmail.com\",\"author\":\"Bahadin\",\"message\":\"initialize VuePress site with basic configuration and deploy workflow\"}]},\"filePathRelative\":\"projects.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
